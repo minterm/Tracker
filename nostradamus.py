22b4c690f277404b4ee05c5b78f2e5ba84c571cb
Sireku <Sireku@users.noreply.github.com>
2017-07-18 13:28:19 -0700

    Update nostradamus.py

--- nostradamus.py
+++ nostradamus.py
@@ -13,10 +13,10 @@ CUBESATS = "http://www.celestrak.com/NORAD/elements/cubesat.txt"
 
 ################################################################################
 class Station():
-    def __init__(self, name=None, location=("0","0",0), callsign=None): 
-	self.name = name
-	self.location = ephem.Observer()
-        # location = (latitude, longitude, elevation) 
+    def __init__(self, name=None, location=("0","0",0), callsign=None):
+        self.name = name
+        self.location = ephem.Observer()
+        # location = (latitude, longitude, elevation)
         #lat and long weren't being set, issue resolved
         if (name.upper() == "KNUDSEN"):
             self.location.lat        = "34.07099"
@@ -32,7 +32,7 @@ class Station():
 ################################################################################
 # TODO: inherit from ephem Body!!!!!
 class Satellite(object):
-    def __init__(self, body, name="FIREBIRD 4", owner=None, uplink=None, 
+    def __init__(self, body, name="FIREBIRD 4", owner=None, uplink=None,
                  downlink=None, mode=None, callsign=None):
         ''' @param body
                 a PyEphem body
@@ -44,16 +44,16 @@ class Satellite(object):
             #if (name == "FIREBIRD"):
             #    self.name = "FIREBIRD 4"
             self.owner    = "Montana State University"
-            self.uplink   = "" 
-            self.downlink = "437.219 MHz" 
+            self.uplink   = ""
+            self.downlink = "437.219 MHz"
             self.mode     = "19200bps FSK"
             self.callsign = "K7MSU"
         elif (name == "ELFIN" or name == "UCLA"):
             if (name == "UCLA"):
                 self.name = "ELFIN"
             self.owner    = "University of California, Los Angeles"
-            self.uplink   = "144.4 MHz" 
-            self.downlink = "437.45 MHz" 
+            self.uplink   = "144.4 MHz"
+            self.downlink = "437.45 MHz"
             self.mode     = "9k6 uplink, 19k2 downlink"
             self.callsign = "W6YRA"
         else:
@@ -68,19 +68,19 @@ class Satellite(object):
         body = self.body
         body.compute(observer)
         return (degrees(body.az), degrees(body.alt))
-    
+
     def getVelocity(self, observer):
-	body = self.body
-	body.compute(observer)
-	return body.range_velocity/1000
-   
+        body = self.body
+        body.compute(observer)
+        velocity = body.range_velocity / 1000
+        return velocity
+
     def getAzimuth(self, observer):
         '''Returns azimuth in degrees'''
         body = self.body
         body.compute(observer)
         return degrees(body.az)
-   
-        
+
     def getElevation(self, observer):
         '''Returns elevation (above horizon) in degrees'''
         body = self.body
@@ -93,7 +93,7 @@ class Predictor(object):
     def __init__(self, knudsen=True):
         ''' @param knudsen
                 if the Earth Station is Knudsen, use Knudsen
-                else, manually call setStation immediately after Predictor 
+                else, manually call setStation immediately after Predictor
                 creation
         '''
         self._station = None
@@ -109,12 +109,12 @@ class Predictor(object):
 
     def getStation(self):
         if hasattr(self._station, 'name'):
-            return self._station.name 
-        return None 
+            return self._station.name
+        return None
 
     ### Satellite Details ###
 
-    def addSatellite(self, name, owner=None, uplink=None, 
+    def addSatellite(self, name, owner=None, uplink=None,
                      downlink=None, mode=None, callsign=None):
         if (name.upper() == "FIREBIRD"):
             name = "FIREBIRD 4"
@@ -195,17 +195,17 @@ class Predictor(object):
                     l2  = f.readline()
                     l3  = f.readline()
                     sat = (l1, l2, l3)
-		    break
-		f.readline()
+                    break
                 f.readline()
-                l1 = f.readline()    	
+                f.readline()
+                l1 = f.readline()
         return sat
 
 
     ### Performance Functions ###
 
     def position(self, satName, date=None):
-        # date currently set to 'now' unless otherwise inputted   
+        # date currently set to 'now' unless otherwise inputted
         if not date:
             date = time.time()
         self._station.location.date = datetime.datetime.utcfromtimestamp(date)
@@ -217,41 +217,42 @@ class Predictor(object):
         if sat:
             return sat.getPosition(self._station.location)
         return None
-    
-    def velocity(self, satName, date=None):
-	if not date:
-	    date = time.time()
-	self._station.location.date = datetime.datetime.utcfromtimestamp(date)
-	sat = None
-	for s in self._sats:
-	    if (s.name == satName):
-	        sat = s
-		break
-	if sat: 
-	    return sat.getVelocity(self._station.location)
-	return None
 
     def nextpass(self, satName, date=None):
         if not date:
-	    date = time.time()
-	self._station.location.date = datetime.datetime.utcfromtimestamp(date)
-	sat = None
-	for s in self._sats:
-	    if (s.name == satName):
-	        sat = s
-		break
-	if sat:
-	    pass_info = self._station.location.next_pass(sat)
-	    # creates six-element tuple  
-	    # 0 Rise time
-	    # 1 Rise azimuth
-	    # 2 Maximum altitude time
-	    # 3 Maximum altitude
-	    # 4 Set time
-   	    # 5 Set azimuth
-	    return pass_info
+            date = time.time()
+        self._station.location.date = datetime.datetime.utcfromtimestamp(date)
+        sat = None
+        for s in self._sats:
+            if (s.name == satName):
+                sat = s
+                break
+        if sat:
+            #change station if necessary
+            return Station("KNUDSEN").location.next_pass(Predictor().loadTLE(satName))
+        # creates six-element tuple
+        # 0 Rise time
+        # 1 Rise azimuth
+        # 2 Maximum altitude time
+        # 3 Maximum altitude
+        # 4 Set time
+           # 5 Set azimuth
+
         return None
-	
+
+    def velocity(self, satName, date=None):
+        if not date:
+            date = time.time()
+        self._station.location.date = datetime.datetime.utcfromtimestamp(date)
+        sat = None
+        for s in self._sats:
+            if (s.name == satName):
+                sat = s
+                break
+        if sat:
+            return sat.getVelocity(self._station.location)
+        return None
+
     def azimuth(self, satName, date=None):
         if not date:
             date = time.time()
@@ -264,7 +265,7 @@ class Predictor(object):
         if sat:
             return sat.getAzimuth(self._station.location)
         return None
-     
+
     def elevation(self, satName, date=None):
         if not date:
             date = time.time()
@@ -281,14 +282,38 @@ class Predictor(object):
 #TODO: what happens when TLE file doesn't exist, when can't update, when
 #      can't read?
 ################################################################################
+'''
 while(1):
     if __name__ == "__main__":
         n = Predictor()
         n.updateTLEs()
         print n.getStation()
-        print n.addSatellite("firebird")
+        print n.addSatellite("FIREBIRD 4")
         print n.getSatellites()
         pos = n.position("FIREBIRD 4")
+        vel = n.velocity("FIREBIRD 4")
+        ptime = n.nextpass("FIREBIRD 4")
+        #info = Station("KNUDSEN").location.next_pass(n.loadTLE("FIREBIRD 4"))
         n.loadTLE("FIREBIRD 4")
         print pos
-        time.sleep(.5)      
+        print vel
+        #print info[0]
+        print ptime[0]
+        time.sleep(1)
+'''
+'''
+while True:
+    W6YRA = ephem.Observer()
+    W6YRA.lon = "-118.441114"
+    W6YRA.lat = "34.07099"
+    W6YRA.elevation = 150
+    date = time.time()
+    W6YRA.date = datetime.datetime.utcfromtimestamp(date)
+    line1 = "FIREBIRD 4"
+    line2 = "1 40378U 15003C   17198.77110422  .00002881  00000-0  12567-3 0  9994"
+    line3 = "2 40378  99.1234  28.8706 0135057 260.9054  97.6871 15.15474522135643"
+    firebird = ephem.readtle(line1, line2, line3)
+    info = W6YRA.next_pass(firebird)
+    print info[0], info[1]
+    time.sleep(1)
+'''
